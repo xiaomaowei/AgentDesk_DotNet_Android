3bd338aa025ea7e08d393c50eb2b6bfba3a264af
xiaomaowei <kawamao@gmail.com>
2026-08-12 22:30:36 +0800

    fix: enlarge secondary dashboard text

--- web-ui/src/main.tsx
+++ web-ui/src/main.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { isWindowsLoopbackDashboard } from './hooks/useHostBridge';
+
+if (isWindowsLoopbackDashboard()) {
+  document.documentElement.classList.add('windows-loopback-dashboard');
+} else {
+  document.documentElement.classList.remove('windows-loopback-dashboard');
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
